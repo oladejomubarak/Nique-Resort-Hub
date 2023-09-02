b897fcce7b4504b377bcbcf914fa392c31a2be30
oladejomubarak <oladejomubarakade@gmail.com>
2023-09-02 04:35:13 +0100

    setting up models

---
+++ src/main/java/oladejo/mubarak/NiqueResortHub/data/model/PaymentStatus.java
@@ -0,0 +1,5 @@
+package oladejo.mubarak.NiqueResortHub.data.model;
+
+public enum PaymentStatus {
+    PENDING, SUCCESSFUL, FAILED
+}
---
+++ src/main/java/oladejo/mubarak/NiqueResortHub/data/model/Room.java
@@ -0,0 +1,31 @@
+package oladejo.mubarak.NiqueResortHub.data.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+@Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String roomNumber;
+    @Enumerated(EnumType.STRING)
+    private RoomType roomType;
+    private BigDecimal roomPrice;
+    @Enumerated(EnumType.STRING)
+    private RoomStatus roomStatus;
+
+    public Room(String roomNumber, RoomType roomType, BigDecimal roomPrice, RoomStatus roomStatus) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.roomStatus = roomStatus;
+    }
+
+}
